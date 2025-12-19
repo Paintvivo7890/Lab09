4d24e243d7c7d0560305ccf0231d297ae4ab693e
Ppaint <narudee_sornsawat@cmu.ac.th>
2025-12-20 01:31:21 +0700

    lab9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 //Write the function printO() here
+#include<iostream>
+using namespace std;
+
+void printO(int N, int M) {
+    if (N <= 0 || M <= 0) {
+        std::cout << "Invalid input" << std::endl;
+        return;
+    }
+    
+    for (int i = 0; i < N; ++i) { 
+        for (int j = 0; j < M; ++j) { 
+            std::cout << "O";
+        }
+        std::cout << std::endl; 
+    }
+}
 
 int main(){
 	
